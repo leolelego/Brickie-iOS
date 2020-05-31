bc82e5a0be74b4f6054709e71c10f3a3984d961a
leolelego <leolelego@pm.me>
2020-05-31 15:20:06 +0200

    Resolve #18 , Resolve #15

--- sources/views/Minifig/MinifigCell.swift
+++ sources/views/Minifig/MinifigCell.swift
@@ -20,8 +20,7 @@ struct MinifigCell: View {
 
             .modifier(RoundedShadowMod())
             VStack(alignment: .leading, spacing: 8) {
-                Rectangle().fill(Color.background).frame(height:16)
-                Text(minifig.name ?? minifig.minifigNumber).font(.headline)
+                Text(minifig.name ?? minifig.minifigNumber).font(.headline).padding(.top,16)
                 ForEach(minifig.subthemes, id: \.self){ sub in
                     Text(sub).font(.subheadline).foregroundColor(Color.gray)
                 }
